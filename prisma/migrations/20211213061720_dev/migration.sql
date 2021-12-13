b56e03070d8d304b83a1d88eade5c4a1a69c707c
Shahan <shahanahmed86@github.com>
2021-12-13 11:21:29 +0500

    initial commit of bootstrap

---
+++ prisma/migrations/20211213061720_dev/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "AdminRole" AS ENUM ('SUPER_ADMIN', 'ADMIN');
+
+-- AlterTable
+ALTER TABLE "Admin" ADD COLUMN     "role" "AdminRole" NOT NULL DEFAULT E'ADMIN';
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
